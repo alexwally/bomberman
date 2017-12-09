ddaa8b288561ddb0e0bd30ed73aaced66fa83a49
alexwally <parfen.a.v@gmail.com>
2017-12-09 12:32:34 +0300

    Some unuseful fix

--- gameserver/src/main/java/ru/atom/gameserver/gamesession/Games.java
+++ gameserver/src/main/java/ru/atom/gameserver/gamesession/Games.java
@@ -1,10 +1,11 @@
 package ru.atom.gameserver.gamesession;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Games {
-    private static ConcurrentHashMap<Integer, GameSession> map = new ConcurrentHashMap<>();
+    private static Map<Integer, GameSession> map = new ConcurrentHashMap<>();
 
     public static void add(Integer id) {
         map.put(id, new GameSession(id));
