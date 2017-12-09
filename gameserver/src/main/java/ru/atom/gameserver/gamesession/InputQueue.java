c62a3e2abb4cd6ad5b09dd2dea6b58054b1aed8c
alexwally <parfen.a.v@gmail.com>
2017-12-09 12:28:34 +0300

    Implemented InputQueue

--- gameserver/src/main/java/ru/atom/gameserver/gamesession/InputQueue.java
+++ gameserver/src/main/java/ru/atom/gameserver/gamesession/InputQueue.java
@@ -1,4 +1,12 @@
 package ru.atom.gameserver.gamesession;
 
+import ru.atom.gameserver.connectionhandler.Message;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class InputQueue {
+    private static Queue<Message> instance = new ConcurrentLinkedQueue<>();
+
+    public static Queue<Message> getInstance() {return instance;}
 }
